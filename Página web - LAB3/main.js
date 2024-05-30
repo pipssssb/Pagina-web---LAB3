e1d072e0c2610b0541bfbee3455b50f68eaeaf2e
Victoria Bonada <pipivick2@gmail.com>
2024-05-30 14:40:09 -0300

    Arrays de productos en JS

---
+++ Página web - LAB3/main.js
@@ -0,0 +1,174 @@
+// PRODUCTOS
+const productos = [
+
+    // Nuevos ingresos
+    
+    {
+        id: "libro-01",
+        titulo: "Naci para esto",
+        imagen: "./imagenes/libros/npe-a01.png",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 22000
+    },
+    {
+        id: "libro-02",
+        titulo: "El problema de los 3 cuerpos",
+        imagen: "./imagenes/libros/epdl3c - cl.jpg",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 27000
+    },
+    {
+        id: "libro-03",
+        titulo: "Dune",
+        imagen: "./imagenes/libros/dune -fh.jpg",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 28100
+    },
+    {
+        id: "libro-04",
+        titulo: "Los juegos del hambre - Libro 4",
+        imagen: "./imagenes/libroos/ljdhbdpcys - sc.jpg",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 26300
+    },
+
+     // Bestseller Argentina
+     
+    {
+        id: "libro-05",
+        titulo: "Heartless",
+        imagen: "./imagenes/libros/heartless - mm.jpg",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 19800
+    },
+    {
+        id: "libro-06",
+        titulo: "Alas de sangre",
+        imagen: "./imagenes/libros/ads - ry.png",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 26900
+    },
+    {
+        id: "libro-07",
+        titulo: "Harry Potter y la piedra filosofal",
+        imagen: "./imagenes/libros/hpylpf - jkr.png",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 52500
+    },
+    {
+        id: "libro-08",
+        titulo: "Buenos presagios",
+        imagen: "./imagenes/libros/bp - tg&ng.jpg",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 26300
+    },
+
+    //Juveniles
+
+    {
+        id: "libro-09",
+        titulo: "Los Juegos del Hambre",
+        imagen: "./imagenes/libros/ljdh - sc.jpg",
+        categoria: {
+            nombre: "Libross",
+            id: "libros"
+        },
+        precio: 26.400
+    },
+    {
+        id: "libro-10",
+        titulo: "Maze Runner: La Cura Mortal",
+        imagen: "./imagenes/libros/mrlcm - jd.png",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 16.500
+    },
+    {
+        id: "libro-11",
+        titulo: "Divergente",
+        imagen: "./imagenes/libros/divergente - vr.jpg",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 26.400
+    },
+    {
+        id: "l-bro012",
+        titulo: "Cazadores de Sombras",
+        imagen: "./imagenes/libros/cdscdh - cc.png",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 23.300
+    },
+    // Comics y novelas graficas
+    {
+        id: "libro-13",
+        titulo: "Invincible compendium one",
+        imagen: "./imagenes/libros/ico - rk&.png",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 55.400
+    },
+    
+    {
+        id: "libro-14",
+        titulo: "Hellboy 1",
+        imagen: "./imagenes/libros/hellboy - mm.jpg",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 35.100
+    },
+    {
+        id: "libro-15",
+        titulo: "Gotham Central",
+        imagen: "./imagenes/libros/gc - eb&gr.jpg",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 75.600
+    },
+    {
+        id: "libro-16",
+        titulo: "Dragon Ball Super 12",
+        imagen: "./imagenes/libros/dbs12 - at.jpg",
+        categoria: {
+            nombre: "Libros",
+            id: "libros"
+        },
+        precio: 5.310
+    },
+];
